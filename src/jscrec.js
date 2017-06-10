164601da3d8bdfc3f8103e4943b3a94336f67487
Caishun Chen <cschen1205@gmail.com>
2017-06-10 21:54:46 +0800

    implementation

--- src/jscrec.js
+++ src/jscrec.js
@@ -2,7 +2,50 @@ var jscrec = jscrec || {};
 
 (function (jss) {
     'use strict';
-	
+    var Tuple2 = new function(p1, p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    };
+    
+    jss.Tuple2 = Tuple2;
+    
+	jss.fitAndTransform = function(adj) {
+        var mapper = {};
+        for(var person in adj) {
+            var friends = adj[person];
+            for(var i = 0; i < friends.length; ++i){
+                var p1 = friends[i];
+                var p2 = person;
+                if(p1 < p2) {
+                    mapper[p1+"_" +p2] = null;
+                } else {
+                    mapper[p2+"_" +p1] = null;  
+                }
+            }
+            for(var i=0; i < friends.length; ++i) {
+                for(var j = i+1; j < friends.length; ++j) {
+                    var p1 = friends[i];
+                    var p2 = friends[j];
+                    
+                    var key = p1 + "_" + p2;
+                    if(p1 > p2) {
+                        key = p2 + "_" + p1;
+                    }
+                    if(key in mapper){
+                        if(mapper[key] == null) {
+                            continue;
+                        } else {
+                            mapper[key].push(person);
+                        }
+                    } else {
+                        mapper[key] = [];
+                    }
+                }
+            }
+        }
+        
+        return mapper;
+    };
     
 })(jscrec);
 
---
+++ test/connection-recommender-spec.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect;
+var jscrec = require("../src/jscrec");
+
+describe("Connection Recommender", function(){
+   it("should be able to recommend common connnection to connect two unknown persons", function(){
+       
+   }) ;
+});
